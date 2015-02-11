f3a5781abcd6cff74f98c765b92c3fa5390d2e09
Sokolov Oleg <voshod-nerd@hotmail.com>
2015-02-11 17:43:10 +0600

    First Version

---
+++ WpfApplication1/Report1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Логика взаимодействия для Report1.xaml
+    /// </summary>
+    public partial class Report1 : Window
+    {
+        public Report1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
